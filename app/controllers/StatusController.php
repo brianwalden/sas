b3276582cae07a9081a11ddabe3ed51b660bcf56
Brian Walden <brianwalden@outlook.com>
2015-06-26 12:38:05 -0400

    update StatusController to use APP_PATH constant

--- app/controllers/StatusController.php
+++ app/controllers/StatusController.php
@@ -4,20 +4,19 @@ namespace Brianwalden\SAS\Controllers;
 
 class StatusController extends BaseController
 {
-    const DIR_BASE = '/home/brianwalden/sas';
-
     public function indexAction()
     {
+        $appPath = rtrim(APP_PATH, '/');
         $message = '';
         $exception = isset($this->params[1]) ? $this->params[1] : null;
         
         if ($exception) {
             $message = str_replace(
-                static::DIR_BASE . '/public',
+                "$appPath/public",
                 '',
                 (string) $exception
             );
-            $message = str_replace(static::DIR_BASE, '/..', $message);
+            $message = str_replace($appPath, '/..', $message);
         }
 
         $this->view->setvars([
